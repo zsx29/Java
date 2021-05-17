9f2fcae756f149c259a2a1abba60bd29357bcbeb
zsx29 <zsx29@naver.com>
2021-05-17 11:36:04 +0900

    2021-05-17 Java Ch01

---
+++ Ch01/src/sub1/HelloWorld.java
@@ -0,0 +1,16 @@
+package sub1;
+
+/*
+ * 날짜 : 2021-05-17
+ * 이름 : 박재형
+ * 내용 : HelloWorld 출력하기
+ */
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("hello world~!");
+		
+	}
+}
